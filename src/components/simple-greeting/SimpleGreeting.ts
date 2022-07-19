63ad429b93d7ad7b29ded9fe9ecfc5e5da981feb
David Vaquero <pepesan@gmail.com>
2022-07-19 17:55:36 +0200

    metido nuevo componente

---
+++ src/components/simple-greeting/SimpleGreeting.ts
@@ -0,0 +1,28 @@
+import {LitElement, html, css} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {MyElement} from "../../my-element";
+
+@customElement('simple-greeting')
+export class SimpleGreeting extends LitElement {
+
+    static override styles = css`
+    
+  `;
+
+    @property({type: Number})
+    aNumber = 5;
+
+    override render() {
+        return html`
+      <h1>Mi first Lit Element</h1>
+      <h2>Number: ${this.aNumber}</h2>
+      <slot></slot>
+    `;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'simple-greeting': MyElement;
+    }
+}
